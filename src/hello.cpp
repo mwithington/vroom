caa46012c95055cd93f0d79b6a9e2ee9c842e21b
demarcot <tom_demarco1013@yahoo.com>
2023-08-09 19:02:30 -0400

    feat(Tom): copy paste opengl hello triange. also sad glfw time :(

--- src/hello.cpp
+++ src/hello.cpp
@@ -26,6 +26,18 @@
 #include "engine/entity/entity.h"
 #include "engine/input/Keyboard.h"
 
+const char *vertexShaderSource = "#version 330 core\n"
+    "layout (location = 0) in vec3 aPos;\n"
+    "void main()\n"
+    "{\n"
+    "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+    "}\0";
+const char *fragmentShaderSource = "#version 330 core\n"
+    "out vec4 FragColor;\n"
+    "void main()\n"
+    "{\n"
+    "   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+    "}\n\0";
 
 // This example is taken from http://learnopengl.com/
 // http://learnopengl.com/code_viewer.php?code=getting-started/hellowindow2
@@ -38,7 +50,7 @@ void glfwErrorCallback(int error, const char* description);
 void handleInput();
 void update(std::vector<Entity*> entityList, double gameTimeElapsed);
 // void render(GLFWwindow* window, Camera* cam, std::vector<Entity*> entityList);
-void render(GLFWwindow* window);
+void render(GLFWwindow* window, unsigned int shaderProgram, unsigned int VAO);
 
 int main()
 {
@@ -126,16 +138,95 @@ int main()
     std::cout << "time: " << micros << std::endl;
 
     // Game loop
-    uint32_t lastTime = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now().time_since_epoch()).count();
+    // uint32_t lastTime = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now().time_since_epoch()).count();
+    uint32_t lastTime = (float)glfwGetTime();
     uint32_t currentTime = 0;
     double gameTimeElapsed = 0;
     std::this_thread::sleep_for(std::chrono::milliseconds(10));
     std::cout << "clock: " << std::clock() << std::endl;
     std::cout << "clocks per sec: " << CLOCKS_PER_SEC << std::endl;
+    std::cout << "glfw time: " << (float)glfwGetTime() << std::endl;
+
+        // vertex shader
+    unsigned int vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+    glCompileShader(vertexShader);
+    // check for shader compile errors
+    int success;
+    char infoLog[512];
+    glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+    if (!success)
+    {
+        glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+        std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
+    }
+    // fragment shader
+    unsigned int fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+    glCompileShader(fragmentShader);
+    // check for shader compile errors
+    glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+    if (!success)
+    {
+        glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
+        std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" << infoLog << std::endl;
+    }
+    // link shaders
+    unsigned int shaderProgram = glCreateProgram();
+    glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram, fragmentShader);
+    glLinkProgram(shaderProgram);
+    // check for linking errors
+    glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+    if (!success) {
+        glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
+        std::cout << "ERROR::SHADER::PROGRAM::LINKING_FAILED\n" << infoLog << std::endl;
+    }
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader);
+
+    float vertices[] = {
+         0.5f,  0.5f, 0.0f,  // top right
+         0.5f, -0.5f, 0.0f,  // bottom right
+        -0.5f, -0.5f, 0.0f,  // bottom left
+        -0.5f,  0.5f, 0.0f   // top left
+    };
+    unsigned int indices[] = {  // note that we start from 0!
+        0, 1, 3,  // first Triangle
+        1, 2, 3   // second Triangle
+    };
+    unsigned int VBO, VAO, EBO;
+    glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &VBO);
+    glGenBuffers(1, &EBO);
+    // bind the Vertex Array Object first, then bind and set vertex buffer(s), and then configure vertex attributes(s).
+    glBindVertexArray(VAO);
+
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+
+    // note that this is allowed, the call to glVertexAttribPointer registered VBO as the vertex attribute's bound vertex buffer object so afterwards we can safely unbind
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+    // remember: do NOT unbind the EBO while a VAO is active as the bound element buffer object IS stored in the VAO; keep the EBO bound.
+    //glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+
+    // You can unbind the VAO afterwards so other VAO calls won't accidentally modify this VAO, but this rarely happens. Modifying other
+    // VAOs requires a call to glBindVertexArray anyways so we generally don't unbind VAOs (nor VBOs) when it's not directly necessary.
+    glBindVertexArray(0);
+
+    vertices[0] -= 0.00001f;
 
     while (!glfwWindowShouldClose(window))
     {
-      currentTime = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now().time_since_epoch()).count();
+      // currentTime = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now().time_since_epoch()).count();
+      currentTime = (float)glfwGetTime() * 1000;
       gameTimeElapsed = (currentTime - lastTime);
 
       // std::cout << "itz tiiiime: " << currTimeMsEpoch << std::endl;
@@ -144,7 +235,7 @@ int main()
       update(entityList, gameTimeElapsed);
 
       // TODO(Tom): create timer loop to limit redraws (maybe a config flag for uncapped fps)
-      render(window);
+      render(window, shaderProgram, VAO);
       lastTime = currentTime;
 
       std::this_thread::sleep_for(std::chrono::milliseconds(10));
@@ -183,7 +274,7 @@ void update(std::vector<Entity*> entityList, double gameTimeElapsed) {
   }
 }
 
-void render(GLFWwindow* window) {
+void render(GLFWwindow* window, unsigned int shaderProgram, unsigned int VAO) {
   // TODO(Tom): have some global counter and only render if it's been >1/60th of a second since last frame
 
 
@@ -192,6 +283,10 @@ void render(GLFWwindow* window) {
   glClear(GL_COLOR_BUFFER_BIT);
 
   // Render world entities
+  glUseProgram(shaderProgram);
+  glBindVertexArray(VAO); // seeing as we only have a single VAO there's no need to bind it every time, but we'll do so to keep things a bit more organized
+                          //glDrawArrays(GL_TRIANGLES, 0, 6);
+  glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
   // Swap the screen buffers
   glfwSwapBuffers(window);
